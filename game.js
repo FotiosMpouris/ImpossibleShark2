9151edef1ae8d135c060ff529e91ccfbfe3f085e
FotiosMpouris <126421111+FotiosMpouris@users.noreply.github.com>
2024-10-19 15:37:34 -0400

    Update game.js

--- game.js
+++ game.js
@@ -54,28 +54,23 @@ for (let i = 1; i <= 3; i++) {
     punchEffects.push(img);
 }
 
-// Mickey characters
-const mickeyPool = [];
-const MAX_MICKEYS = 5;
-
-function createMickey() {
-    return {
-        x: canvas.width,
-        y: canvas.height - GROUND_HEIGHT - 400,
-        width: 900,
-        height: 500,
-        frameX: 0,
-        speed: 2,
-        visible: true,
-        state: 'walking',
-        hitDuration: 0,
-        staggerDistance: 50,
-        hitboxWidth: 100,
-        hitboxOffset: 400,
-        punchEffectIndex: -1,
-        punchEffectDuration: 0
-    };
-}
+// Mickey character
+const mickey = {
+    x: canvas.width,
+    y: canvas.height - GROUND_HEIGHT - 400,
+    width: 900,
+    height: 500,
+    frameX: 0,
+    speed: 2,
+    visible: true,
+    state: 'walking',
+    hitDuration: 0,
+    staggerDistance: 50,
+    hitboxWidth: 100,
+    hitboxOffset: 400,
+    punchEffectIndex: -1,
+    punchEffectDuration: 0
+};
 
 // Mickey's walking animation
 const mickeyImages = [];
@@ -116,11 +111,11 @@ function gameLoop() {
 
     updateBackground();
     updateEric();
-    updateMickeys();
+    updateMickey();
 
     drawBackground();
     drawEric();
-    drawMickeys();
+    drawMickey();
     drawScore();
 
     gameSpeed += 0.0001;
@@ -152,63 +147,53 @@ function updateEric() {
     frameCount++;
 }
 
-function updateMickeys() {
-    // Spawn new Mickeys based on game speed
-    if (mickeyPool.length < Math.min(MAX_MICKEYS, Math.floor(gameSpeed))) {
-        mickeyPool.push(createMickey());
-    }
-
-    mickeyPool.forEach((mickey, index) => {
-        if (mickey.visible) {
-            switch(mickey.state) {
-                case 'walking':
-                    mickey.x -= mickey.speed * gameSpeed;
-                    if (frameCount % 5 === 0) {
-                        mickey.frameX = (mickey.frameX + 1) % 21;
-                    }
-                    // Check if Mickey walked past Eric
-                    if (mickey.x + mickey.hitboxOffset < eric.x) {
-                        curseSound.play().catch(e => console.error("Error playing curse sound:", e));
-                        respawnMickey(mickey);
-                    }
-                    break;
-                case 'hit':
-                    mickey.hitDuration++;
-                    if (mickey.hitDuration <= 30) {
-                        mickey.x += mickey.speed * 0.5; // Stagger backwards
-                    } else if (mickey.hitDuration > 60) {
-                        mickey.state = 'walking';
-                        mickey.hitDuration = 0;
-                        mickey.punchEffectIndex = -1;
-                    }
-                    break;
-            }
-
-            // Check for collision with Eric's punch
-            if (eric.punching && 
-                eric.punchDuration >= 5 && eric.punchDuration <= 20 && 
-                mickey.state === 'walking' &&
-                mickey.x + mickey.hitboxOffset < eric.x + eric.punchHitboxOffset + eric.punchHitboxWidth &&
-                mickey.x + mickey.hitboxOffset + mickey.hitboxWidth > eric.x + eric.punchHitboxOffset) {
-                hitMickey(mickey);
-            }
-
-            // Update punch effect
-            if (mickey.punchEffectIndex !== -1) {
-                mickey.punchEffectDuration++;
-                if (mickey.punchEffectDuration > 30) {
-                    mickey.punchEffectIndex = -1;
-                    mickey.punchEffectDuration = 0;
+function updateMickey() {
+    if (mickey.visible) {
+        switch(mickey.state) {
+            case 'walking':
+                mickey.x -= mickey.speed * gameSpeed;
+                if (frameCount % 5 === 0) {
+                    mickey.frameX = (mickey.frameX + 1) % 21;
                 }
+                // Check if Mickey walked past Eric
+                if (mickey.x + mickey.hitboxOffset < eric.x) {
+                    curseSound.play().catch(e => console.error("Error playing curse sound:", e));
+                    respawnMickey();
+                }
+                break;
+            case 'hit':
+                mickey.hitDuration++;
+                if (mickey.hitDuration <= 30) {
+                    mickey.x += mickey.speed * 0.5; // Stagger backwards
+                } else if (mickey.hitDuration > 60) {
+                    mickey.state = 'walking';
+                    mickey.hitDuration = 0;
+                    mickey.punchEffectIndex = -1;
+                }
+                break;
+        }
+
+        // Check for collision with Eric's punch
+        if (eric.punching && 
+            eric.punchDuration >= 5 && eric.punchDuration <= 20 && 
+            mickey.state === 'walking' &&
+            mickey.x + mickey.hitboxOffset < eric.x + eric.punchHitboxOffset + eric.punchHitboxWidth &&
+            mickey.x + mickey.hitboxOffset + mickey.hitboxWidth > eric.x + eric.punchHitboxOffset) {
+            hitMickey();
+        }
+
+        // Update punch effect
+        if (mickey.punchEffectIndex !== -1) {
+            mickey.punchEffectDuration++;
+            if (mickey.punchEffectDuration > 30) {
+                mickey.punchEffectIndex = -1;
+                mickey.punchEffectDuration = 0;
             }
         }
-    });
-
-    // Remove off-screen Mickeys
-    mickeyPool.filter(mickey => mickey.x + mickey.width < 0);
+    }
 }
 
-function hitMickey(mickey) {
+function hitMickey() {
     mickey.state = 'hit';
     mickey.hitDuration = 0;
     mickeyNoises[currentMickeyNoiseIndex].play().catch(e => console.error("Error playing Mickey noise:", e));
@@ -218,8 +203,8 @@ function hitMickey(mickey) {
     score++;
 }
 
-function respawnMickey(mickey) {
-    mickey.x = canvas.width + Math.random() * 500; // Add some randomness to respawn position
+function respawnMickey() {
+    mickey.x = canvas.width;
     mickey.visible = true;
     mickey.state = 'walking';
     mickey.speed += 0.1;
@@ -239,24 +224,22 @@ function drawEric() {
     }
 }
 
-function drawMickeys() {
-    mickeyPool.forEach(mickey => {
-        if (mickey.visible) {
-            if (mickey.state === 'walking') {
-                ctx.drawImage(mickeyImages[mickey.frameX], mickey.x, mickey.y, mickey.width, mickey.height);
-            } else if (mickey.state === 'hit') {
-                ctx.drawImage(mickeyHitImage, mickey.x, mickey.y, mickey.width, mickey.height);
-            }
-
-            // Draw punch effect
-            if (mickey.punchEffectIndex !== -1) {
-                ctx.drawImage(punchEffects[mickey.punchEffectIndex], 
-                    mickey.x + mickey.width / 2 - 100, 
-                    mickey.y + mickey.height / 2 - 100, 
-                    200, 200);
-            }
+function drawMickey() {
+    if (mickey.visible) {
+        if (mickey.state === 'walking') {
+            ctx.drawImage(mickeyImages[mickey.frameX], mickey.x, mickey.y, mickey.width, mickey.height);
+        } else if (mickey.state === 'hit') {
+            ctx.drawImage(mickeyHitImage, mickey.x, mickey.y, mickey.width, mickey.height);
         }
-    });
+
+        // Draw punch effect
+        if (mickey.punchEffectIndex !== -1) {
+            ctx.drawImage(punchEffects[mickey.punchEffectIndex], 
+                mickey.x + mickey.width / 2 - 100, 
+                mickey.y + mickey.height / 2 - 100, 
+                200, 200);
+        }
+    }
 }
 
 function drawScore() {
